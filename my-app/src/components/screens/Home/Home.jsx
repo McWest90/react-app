5ce68f1f0b6ffeec75057526617861f46dffd79f
stas <Mcwest90@gmail.com>
2023-10-15 18:22:47 +0300

    start

---
+++ my-app/src/components/screens/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const todos = [
+    {
+        title: 'Finish the essay collaboration',
+        isComplete
+    }
+]
+
+const Home = () => {
+    return (
+        <div className="bg-gray-900 h-screen text-white">
+            Home
+        </div>
+    )
+}
+
+
+export default Home
